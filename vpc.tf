25419e0a7a605ff223cee591f50cb5b58a86db09
breno500as <breno500as@gmail.com>
2024-01-16 10:17:38 -0300

    up

---
+++ vpc.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "vpc-terraform-${terraform.workspace}"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  count = terraform.workspace == "prod" ? 2 : 1
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.${count.index}.0/24"
+
+  tags = {
+    Name = "subnet-terraform-${terraform.workspace}-${count.index}"
+  }
+}
+
+
+
+ 
